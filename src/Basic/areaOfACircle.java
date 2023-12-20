a31b8048b2fcb32e9e6b894e61ac0f3272856a4b
Tanjim <tanjimseo@gmail.com>
2023-12-21 01:56:01 +0530

    Solved Area Of Circle

---
+++ src/Basic/areaOfACircle.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class areaOfACircle {
+    public static void main(String[] args) {
+        double r = 3.9; // radius
+        double pi = 3.14; // pi = 3.14
+        double area = pi*r*r;
+        System.out.println("Area of circle with radius "+r+" is " + area);
+    }
+}
