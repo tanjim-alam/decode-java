d624fff9c50b3659e48209d7dace3358603ea7c0
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2024-01-25 02:05:47 +0530

    Two sum and Two Pointer

--- src/Arrays/arraysQuestion.java
+++ src/Arrays/arraysQuestion.java
@@ -2,11 +2,54 @@ package Arrays;
 
 public class arraysQuestion {
     public static void main(String[] args) {
+
+        /*
         int[] arr = {81,17,45,36,31,100};
         for (int i=0; i<arr.length; i++){
           if (arr[i]<35){
               System.out.println("Student roll number is: " + i);
           }
         }
+         */
+
+        // Two Sum
+        /*
+        int[] arr = {2,3,4,5,6,7,3};
+        int target = 7;
+        twoSum(arr,target);
+
+         */
+
+        // reverse array using two pointer
+        int[] arr = {2,3,4,5,6,7,3};
+        reverse(arr);
     }
+
+//    public static void twoSum(int[] arr, int target){
+//        for (int i=0; i<arr.length; i++){
+//            for (int j=i+1; j<arr.length; j++){
+//                if (arr[i] + arr[j] == target){
+//                    System.out.println(i + " " +j + "res");
+//                    break;
+//                }
+//            }
+//        }
+//    }
+
+    public static void reverse(int[] arr) {
+        int i=0;
+        int j = arr.length-1;
+        while ( i< j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        for (int ele : arr){
+            System.out.print(ele + " ");
+
+        }
+    }
+
 }
